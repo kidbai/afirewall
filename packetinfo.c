5f6aa2b4f84410b4c4d45ca0e9fdddc4ec5ddd72
Jan Christian Kaessens <post@janniweb.de>
2012-02-08 22:16:11 +0100

    changed license from CC-BY-SA to 3-clause BSD

--- packetinfo.c
+++ packetinfo.c
@@ -1,9 +1,30 @@
 /*
- * packetinfo.c
- *
- *  Created on: 06.12.2011
- *      Author: jan
- */
+Copyright (c) 2012, Jan Christian Kaessens
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the organization nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL JAN CHRISTIAN KAESSENS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -248,6 +269,7 @@ void pi_print(struct packet_info *pinfo) {
 	 * to IP version.
 	 */
 	switch(pinfo->network_header_type) {
+
 	case IP:
 		printf("outgoing packet: interface: %s network: IP transport: %s source: %s:%u destination: %s:%u\n",
 				pinfo->interface, trans,
@@ -261,12 +283,14 @@ void pi_print(struct packet_info *pinfo) {
 		sockfd2process(pinfo, inode);
 		printf("inode: %d, pid: %d, cmd: %s\n", inode, pinfo->process_id, pinfo->process_cmd);
 		break;
+
 	case IP6:
 		printf("outgoing packet: interface: %s network: IP6 transport: %s source: [%s]:%u destination: [%s]:%u\n",
 				pinfo->interface, trans,
 				pinfo->local_addr, pinfo->local_port, pinfo->remote_addr, pinfo->remote_port);
 		break;
 	default:
+
 		printf("outgoing packet: interface: %s network: %s transport: %s\n",
 				pinfo->interface, net, trans);
 	}
